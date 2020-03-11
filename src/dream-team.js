f7499e636c634ae3f55b6610b995ee7a25c9a679
lolmaster277 <45518100+lolmaster277@users.noreply.github.com>
2020-03-11 23:41:04 +0500

    init: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+
+    if (!Array.isArray(arr)) {
+        return false;
+    } else {
+        var answer = "";
+        for (var j = 0; j < arr.length; j++) {
+
+            if (typeof arr[j] == "string") {
+                for (var i = 0; i < arr[j].length; i++) {
+                    if (arr[j][i] != " ") {
+                        answer = answer + (arr[j][i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return answer.toUpperCase().split("").sort().join("");
+
+    }
 };
