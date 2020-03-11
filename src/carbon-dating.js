0715748f3d05d89a4211a33e3d1d0e85bf1e7859
lolmaster277 <45518100+lolmaster277@users.noreply.github.com>
2020-03-11 22:45:18 +0500

    init: sol

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function dateSample(sampleActivity) {
+    if ("string" != typeof(sampleActivity) || isNaN(parseFloat(sampleActivity))) {
+        return false;
+    }
+    var result = 0;
+    var k = 0.693 / HALF_LIFE_PERIOD;
+    result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k);
+    if (result > 0 && result != Infinity)
+        return result;
+    else
+        return false;
 };
