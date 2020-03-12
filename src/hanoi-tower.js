f13beafd5002bbabe815a606f5d8941a7bc4ddd1
lolmaster277 <45518100+lolmaster277@users.noreply.github.com>
2020-03-12 11:39:01 +0500

    feat: hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,8 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    console.log(disksNumber);
+    return {
+        'turns ': (Math.pow(2, disksNumber) - 1),
+        'seconds': (Math.pow(2, disksNumber) - 1) / turnsSpeed * 3600
+    }
+
 }
