a586140a78fe3f06107e40c3daa8c118db401042
lolmaster277 <45518100+lolmaster277@users.noreply.github.com>
2020-03-12 15:25:57 +0500

    feat: chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,39 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    elements: [],
+    getLength() {
+        return this.elements.length;
+    },
+    addLink(value) {
+        this.elements.push(String(value));
+        return this;
+    },
+    removeLink(position) {
+        position--;
+        if (position >= 0 && position < this.elements.length) {
+            this.elements.splice(position, 1);
+        } else {
+            this.elements = [];
+            throw Error();
+        }
+        return this;
+    },
+    reverseChain() {
+        this.elements.reverse();
+        return this;
+    },
+    finishChain() {
+        let answer = "";
+        for (let i = 0; i < this.elements.length; i++) {
+            if (i == 0) {
+                answer = answer + "( " + this.elements[i] + " )";
+            } else {
+                answer = answer + "~~( " + this.elements[i] + " )";
+            }
+        }
+
+        this.elements = [];
+        return answer;
+    }
 };
 
 module.exports = chainMaker;
