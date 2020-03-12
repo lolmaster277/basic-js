d7f09eebdb3c7ba4375ee9dbd74b799b5903a64a
lolmaster277 <45518100+lolmaster277@users.noreply.github.com>
2020-03-12 12:34:55 +0500

    feat: transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let answer = [];
+    if (!Array.isArray(arr)) {
+        throw Error();
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+
+        switch (arr[i]) {
+            case '--discard-next':
+                if (i + 1 < arr.length)
+                    i = i + 1;
+                break;
+            case '--discard-prev':
+                if (i - 1 >= 0)
+                    answer.pop();
+                break;
+            case '--double-next':
+                if (i + 1 < arr.length)
+                    answer.push(arr[i + 1]);
+                break;
+            case '--double-prev':
+                if (i - 1 >= 0)
+                    answer.push(arr[i - 1]);
+                break;
+            default:
+                answer.push(arr[i]);
+                break;
+        }
+
+    }
+
+    return answer;
+
 };
