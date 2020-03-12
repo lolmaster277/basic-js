f4cb324129d1781b35a6d08f27f1c4b2101e8247
lolmaster277 <45518100+lolmaster277@users.noreply.github.com>
2020-03-12 12:04:51 +0500

    feat: hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,9 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    console.log(disksNumber);
+
 
     return {
-        'turns ': (Math.pow(2, disksNumber) - 1),
-        'seconds': (Math.pow(2, disksNumber) - 1) / turnsSpeed * 3600
-    }
+        'turns': (Math.pow(2, disksNumber) - 1),
+        'seconds': ((Math.pow(2, disksNumber) - 1)) / (turnsSpeed / 3600)
+    };
 
 }
